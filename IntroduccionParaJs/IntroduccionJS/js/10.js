8a7db33a8f11ef7bb899ab5374a1548bbc8cada7
juan jose botero garcía <jjboterog@eafit.edu.co>
2025-06-04 12:38:15 -0500

    Add 10

--- IntroduccionParaJs/IntroduccionJS/js/10.js
+++ IntroduccionParaJs/IntroduccionJS/js/10.js
@@ -1,4 +1,4 @@
-// Agregar a arrays
+/* // Agregar a arrays
 
 const lista = [20, 30, 40, 50];
 lista.push(60); // Agregar al final
@@ -15,3 +15,36 @@ console.log(lista.includes(30)); // true
 const nuevoElemento = 60;
 const nuevaLista = [...lista, nuevoElemento]; // Crear una nueva lista con el nuevo elemento
 console.log(nuevaLista); // [20, 30, 40, 50, 60]
+ */
+
+// Para react las listas se manejan de forma diferente
+// Si quiero agregar un elemento a una lista en react, no se puede modificar la lista original
+// Se debe crear una nueva lista con el nuevo elemento
+
+let listaReact = [20, 30, 40, 50];
+const nuevoElementoReact = 60;
+let listaReact1 = [...listaReact, nuevoElementoReact]; // Crear una nueva lista con el nuevo elemento
+
+console.log(listaReact); // [20, 30, 40, 50, 60]
+
+// Si quiero eliminar un elemento de una lista en react, no se puede modificar la lista original
+const elementoAEliminar = 30;
+let listaReact2 = listaReact1.filter(function(elemento){
+    if (elemento !== elementoAEliminar) {
+        return elemento;
+    }
+})
+
+console.log(listaReact2); // [20, 40, 50, 60]
+
+// Si quiero modificar un elemento de una lista en react, no se puede modificar la lista original
+const elementoAModificar = 70;
+let listaReact3 = listaReact2.map(function(elemento){
+    if (elemento === 60){
+        return elementoAModificar; // Modificar el elemento
+    }
+    else {
+        return elemento; // Dejar el elemento igual
+    }
+})
+console.log(listaReact3); // [20, 40, 50, 70]
